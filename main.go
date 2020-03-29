7409eb0ad3437e6dc5200b1de48e8a3b0aa7efd3
jbpratt78 <jbpratt78@gmail.com>
2020-03-28 22:05:08 -0500

    decorate errors, start keeping up with time it took to answer
    - modularize code
      - needs tests
    - log rather than panic
    - decorate errors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"html"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -44,27 +45,28 @@ type chatterAnswer struct {
 	user   string
 	order  int
 	answer int
+	dur    time.Duration
 }
 
 func main() {
 	var current *result
 	var correct int
+	var start time.Time
+
 	inProgress := false
 	timeup := false
 	players := []chatterAnswer{}
 
-	triviaClient := http.Client{Timeout: time.Second * 2}
-
 	jwt := os.Getenv("STRIMS_TOKEN")
 	if jwt == "" {
-		panic(fmt.Errorf("no jwt provided"))
+		log.Fatal(fmt.Errorf("no jwt provided"))
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	if err := requestToken(); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	c, _, err := websocket.Dial(ctx, addr,
@@ -74,7 +76,7 @@ func main() {
 			},
 		})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer c.Close(websocket.StatusInternalError, "connection closed")
 
@@ -82,24 +84,24 @@ func main() {
 	for {
 		_, data, err := c.Read(ctx)
 		if err != nil {
-			panic(err)
+			log.Fatalf("failed to read from conn: %v", err)
 		}
 
 		msg := strings.SplitN(string(data), " ", 2)
 		var content map[string]interface{}
 		if msg[0] == "MSG" {
+			start = time.Now()
 			if err = json.Unmarshal([]byte(msg[1]), &content); err != nil {
-				panic(err)
+				log.Fatalf("failed to unmarshal msg response: %v %v", msg, err)
 			}
 			chatMsg := content["data"].(string)
 			if strings.HasPrefix(chatMsg, "!trivia") && !inProgress {
-				// check if msg contains category
-				fmt.Println("Starting trivia round")
+				// TODO check if msg contains category
+				fmt.Println("Starting trivia round. Requesting data")
 				inProgress = true
-				fmt.Println("Requesting data")
-				current, err = requestTriviaData(ctx, &triviaClient)
+				current, err = requestTriviaData()
 				if err != nil {
-					panic(err)
+					log.Fatal(err)
 				}
 				answers := []string{current.CorrectAnswer}
 				answers = append(answers, current.IncorrectAnswers...)
@@ -119,33 +121,35 @@ func main() {
 				}
 
 				x := fmt.Sprintf(
-					"Trivia time answer is in 20s, whisper me the number! (%s) Question: `%s`... Possible answers: %s",
-					current.Category, strings.Replace(html.UnescapeString(current.Question), "\"", "'", -1),
-					out,
+					"%s Trivia time answer is in 20s, whisper me the number! (%s) Question: `%s`... Possible answers: %s",
+					randomEmote(), current.Category, strings.Replace(html.UnescapeString(current.Question), "\"", "'", -1), out,
 				)
 
 				initialQuestion := fmt.Sprintf(`MSG {"data": "%s"}`, x)
 				if err = sendMessage(ctx, c, initialQuestion); err != nil {
-					panic(err)
+					log.Fatal(err)
 				}
 				go func() {
+					fmt.Println("sleeping")
 					time.Sleep(20 * time.Second)
 					inProgress = false
 					timeup = true
+					fmt.Println("timeup")
 				}()
 			}
 		} else if msg[0] == "PRIVMSG" && inProgress {
 			if err = json.Unmarshal([]byte(msg[1]), &content); err != nil {
-				panic(err)
+				log.Fatalf("failed to unmarshal msg response: %v %v", msg, err)
 			}
 			present := false
 			user := content["nick"].(string)
 			// check if user has already answered
 			for _, pl := range players {
+				// if we find the user, alert them
 				if user == pl.user {
-					out := fmt.Sprintf(`PRIVMSG {"data": "You have already answered! Sorry.", "nick": %q}`, user)
+					out := fmt.Sprintf(`PRIVMSG {"data": "You have already answered! MiyanoBird", "nick": %q}`, user)
 					if err = sendMessage(ctx, c, out); err != nil {
-						panic(err)
+						log.Fatal(err)
 					}
 					present = true
 					break
@@ -155,13 +159,18 @@ func main() {
 			if !present {
 				ans, err := strconv.ParseInt(content["data"].(string), 0, 32)
 				if err != nil {
-					out := fmt.Sprintf(`PRIVMSG {"data": "Could not determine answer", "nick": %q}`, user)
+					out := fmt.Sprintf(`PRIVMSG {"data": "I could not determine your answer FeelsPepoMan", "nick": %q}`, user)
 					if err = sendMessage(ctx, c, out); err != nil {
-						panic(err)
+						log.Fatal(err)
 					}
 				} else {
 					fmt.Printf("%s is playing with %d\n", user, ans)
-					players = append(players, chatterAnswer{order: len(players) + 1, answer: int(ans), user: user})
+					players = append(players, chatterAnswer{
+						order:  len(players) + 1,
+						answer: int(ans),
+						user:   user,
+						dur:    time.Since(start),
+					})
 				}
 			}
 		}
@@ -169,20 +178,22 @@ func main() {
 		if timeup {
 			fmt.Println("Determining winner...")
 			out := fmt.Sprintf(`MSG {"data": "The correct answer is: %d %s. `, correct, strings.Replace(html.UnescapeString(current.CorrectAnswer), "\"", "'", -1))
-			plus := `No one answered correctly"}`
+			plus := `No one answered correctly PepeLaugh"}`
 			if len(players) > 0 {
 				sort.Slice(players, func(i, j int) bool {
 					return players[i].order < players[j].order
 				})
 				for _, ans := range players {
 					if ans.answer == correct {
-						plus = fmt.Sprintf(`%s won this round"}`, ans.user)
+						// award points based on duration, player count, question difficulty
+						plus = fmt.Sprintf(`%s won this round. They answered in %s"}`, ans.user, ans.dur.String())
 						break
 					}
 				}
 			}
+
 			if err = sendMessage(ctx, c, out+plus); err != nil {
-				panic(err)
+				log.Fatal(err)
 			}
 			timeup = false
 			players = nil
@@ -191,81 +202,99 @@ func main() {
 }
 
 func sendMessage(ctx context.Context, c *websocket.Conn, input string) error {
-	fmt.Println(input)
+	fmt.Printf("Sending: %s\n", input)
 	if err := c.Write(
 		ctx,
 		websocket.MessageText,
 		[]byte(input),
 	); err != nil {
-		return err
+		return fmt.Errorf("failed to send message: %v", err)
 	}
 	return nil
 }
 
 func requestToken() error {
 	// TODO: query param
-	resp, err := http.Get("https://opentdb.com/api_token.php?command=request")
+	body, err := get("https://opentdb.com/api_token.php?command=request")
 	if err != nil {
-		return err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
+		return fmt.Errorf("failed to make get request: %v", err)
 	}
 
 	tokenRes := struct {
 		Token string `json:"token"`
 	}{}
 	if err = json.Unmarshal(body, &tokenRes); err != nil {
-		return err
+		return fmt.Errorf("failed to unmarshal token: %v", err)
 	}
 
-	u, err := url.Parse(triviaURL)
+	triviaURL, err = encodeURL(triviaURL, "token", tokenRes.Token)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to encode url: %v", err)
+	}
+	return nil
+}
+
+func requestTriviaData() (*result, error) {
+	body, err := get(triviaURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make get request: %v", err)
+	}
+	responseData := response{}
+	if err = json.Unmarshal(body, &responseData); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response data: %v", err)
+	}
+
+	if err = filterQuestions(&responseData); err != nil {
+		return nil, err
+	}
+
+	return &responseData.Results[rand.Intn(len(responseData.Results))], nil
+}
+
+func get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func filterQuestions(res *response) error {
+	r, err := regexp.Compile("19[0-9]\\d{1}")
+	if err != nil {
+		return fmt.Errorf("failed to compile regexp: %v", err)
+	}
+
+	for i, trivia := range res.Results {
+		if r.MatchString(trivia.Question) && trivia.Category == "Entertainment: Music" {
+			res.Results[i] = res.Results[0]
+			res.Results = res.Results[1:]
+		}
+	}
+	return nil
+}
+
+func encodeURL(addr, key, value string) (string, error) {
+	u, err := url.Parse(addr)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse url: %v", err)
 	}
 
 	q, err := url.ParseQuery(u.RawQuery)
 	if err != nil {
-		return err
+		return "", fmt.Errorf("failed to parse query: %v", err)
 	}
 
-	q.Add("token", tokenRes.Token)
+	q.Add(key, value)
 	u.RawQuery = q.Encode()
-	triviaURL = u.String()
-	return nil
+	return u.String(), nil
 }
 
-func requestTriviaData(ctx context.Context, client *http.Client) (*result, error) {
-	req, err := http.NewRequest(http.MethodGet, triviaURL, nil)
-	if err != nil {
-		return nil, err
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	responseData := response{}
-	if err = json.Unmarshal(body, &responseData); err != nil {
-		return nil, err
-	}
-
-	r, err := regexp.Compile("19[0-9]\\d{1}")
-	if err != nil {
-		return nil, err
-	}
-
-	for i, trivia := range responseData.Results {
-		if r.MatchString(trivia.Question) && trivia.Category == "Entertainment: Music" {
-			responseData.Results[i] = responseData.Results[0]
-			responseData.Results = responseData.Results[1:]
-		}
-	}
-
-	return &responseData.Results[rand.Intn(len(responseData.Results))], nil
+func randomEmote() string {
+	emotes := []string{"POGGERS", "SOY", "PepoGood", "PepoG", "PepoHmm"}
+	return emotes[rand.Intn(len(emotes))]
 }
